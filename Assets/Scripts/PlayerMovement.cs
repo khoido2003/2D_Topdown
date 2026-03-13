fbc6a38c0cb089df61ade6227fb8f6acfa6306e1
khoido2003 <khoizpro31@gmail.com>
2026-03-13 13:56:53 +0700

    feat: add animation walking and idle

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,16 +1,43 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField]
+    private float moveSpeed = 5f;
+
+    private Animator animator;
+
+    private Rigidbody2D rb;
+
+    private Vector2 moveInput;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+
+        bool isMoving = moveInput.sqrMagnitude > 0.01f;
+
+        animator.SetBool("IsWalking", isMoving);
+
+        animator.SetFloat("CurrentInputX", moveInput.x);
+        animator.SetFloat("CurrentInputY", moveInput.y);
+
+        if (isMoving)
+        {
+            animator.SetFloat("LastInputX", moveInput.x);
+            animator.SetFloat("LastInputY", moveInput.y);
+        }
     }
 }
